911f3d8245fe8d538b0460f3f2afed31e4b7045d
Ravy <ravy@overpro.ru>
2021-09-23 18:42:28 +0300

    test123211

---
+++ components/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="index">
+        {{ $gettext('Приветйййй') }}
+    </div>
+</template>
+
+<script>
+import {Layout} from '@/const/storybook'
+
+export default {
+    name: `${Layout}index`,
+}
+</script>
